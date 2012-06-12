28b391bc858cf1d4ec01cdc03fbac35720509d1f
ouhouhsami <samuel.goldszmidt@gmail.com>
2012-06-12 16:20:34 +0200

    show duration in hour in the player, and add participant filtering possibility

--- archives/filtersets.py
+++ archives/filtersets.py
@@ -1,7 +1,6 @@
+import datetime
 import django_filters
 from archives.models import *
-import datetime
-
 
 
 YEAR_CHOICES = [(x,x) for x in range(1977,datetime.datetime.now().year+1)]
@@ -12,6 +11,7 @@ class AudioFilterSet(django_filters.FilterSet):
     kf_id_lieu = django_filters.ModelChoiceFilter(label="Place", queryset=Lieu.objects.all())
     kf_id_intervenant_principal = django_filters.ModelChoiceFilter(label="Keynote speaker", queryset=Intervenant.objects.all())
     kf_id_orchestre = django_filters.ModelChoiceFilter(label="Orchestra", queryset=Orchestre.objects.all())
+    intervenants = django_filters.ModelMultipleChoiceFilter(label="Participants", queryset=Intervenant.objects.all())
     class Meta:
         model = Audio
-        fields = ['annee', 'kf_id_lieu', 'kf_id_intervenant_principal', 'kf_id_orchestre', 'kf_id_lieu']
+        fields = ['annee', 'kf_id_lieu', 'kf_id_intervenant_principal', 'kf_id_orchestre', 'kf_id_lieu', 'intervenants']
